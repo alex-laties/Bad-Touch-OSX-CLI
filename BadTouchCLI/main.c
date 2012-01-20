57c9306593296b8cebbe2911d9cadd7a33db6649
Alex Laties <alex.laties@gmail.com>
2012-01-19 20:52:12 -0500

    Initial Commit

---
+++ BadTouchCLI/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BadTouchCLI
+//
+//  Created by Alex Laties on 1/19/12.
+//  Copyright (c) 2012 University of Pennsylvania. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
