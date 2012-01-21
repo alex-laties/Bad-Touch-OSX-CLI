ebb5789a1d90d69ff4084e0d3af01ab4aa70640d
Alex Laties <alex.laties@gmail.com>
2012-01-21 15:27:15 -0500

    processing of packets seems to be done. overall processing in the works.

--- BadTouchCLI/SoundAnalyzer.cpp
+++ BadTouchCLI/SoundAnalyzer.cpp
@@ -7,29 +7,69 @@
 //
 #include "SoundAnalyzer.h"
 #include <ctype.h>
+#ifdef __APPLE__
+#include <sys/time.h>
+#endif
 
 void SoundAnalyzer::ProcessPacket(AudioQueueBufferRef buf) {
-  UInt32 val;
+  UInt32 val = 0;
   UInt32 *position = (UInt32 *)buf->mAudioData;
-  UInt32 totalVals = sizeof(buf->mAudioData) / buf->mAudioDataByteSize;
+  UInt32 totalVals = buf->mAudioDataByteSize / 4;
   for( UInt32 i = 0;  i < totalVals; ++i)
   {
     val = *position;
+    if (peaks->empty()) {
+      start_peak_time = last_peak_time = 0;
+    }
+    
+    if (last_peak_time - start_peak_time > 500) {
+      ProcessPeaks();
+    }
+    
     if (val > threshold)
     {
-      
+      unsigned long time = SoundAnalyzer::GetCurrentTime();
+      unsigned long dif = time - last_peak_time;
+      if (start_peak_time == 0) {
+        start_peak_time = time;
+      }
+      if (dif >= 100) {
+        peaks->push_back(val);
+        last_peak_time = time;
+      }
     }
     ++position;
   }
 }
 
-SoundAnalyzer::SoundAnalyzer(UInt32 _threshold){
-  position = 0;
-  last_peak = 0;
-  num_peaks = 0;
-  max_amplitude = 0.;
-  time_since_last = 0.;
-  clip_length = 0.;
-  threshold = _threshold;
+void SoundAnalyzer::ProcessPeaks() {
+  //TODO determining # of peaks/scratching
 }
 
+
+SoundAnalyzer::SoundAnalyzer(UInt32 _threshold, AudioStreamBasicDescription* _format){
+  last_peak_time = 0;
+  max_amplitude = 0.;
+  clip_length = 0.;
+  threshold = _threshold;
+  format = _format;
+  peaks = new std::vector<AUDIO_BLOCK>();
+}
+
+unsigned long SoundAnalyzer::GetCurrentTime() {
+#ifdef __APPLE__
+  timeval current;
+  gettimeofday(&current, NULL);
+  return current.tv_sec * 1000 + current.tv_usec / 1000;
+#endif
+#ifdef _WIN32
+  //TODO windows get time
+#endif
+#ifdef __linux__
+  //TODO linux get time
+#endif
+}
+
+SoundAnalyzer::~SoundAnalyzer() {
+  delete peaks;
+}
--- BadTouchCLI/SoundAnalyzer.h
+++ BadTouchCLI/SoundAnalyzer.h
@@ -11,21 +11,27 @@
 #include <AudioToolbox/AudioToolbox.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+
+#ifndef AUDIO_BLOCK
+#define AUDIO_BLOCK UInt32
+#endif
 
 class SoundAnalyzer {
 private:
-  int position;
-  int last_peak;
-  int num_peaks;
+  unsigned long start_peak_time, last_peak_time;
   float threshold;
   float max_amplitude;
-  float time_since_last;
   float clip_length;
+  AudioStreamBasicDescription* format;
+  std::vector<AUDIO_BLOCK>* peaks;
 public:
   void ProcessPacket(AudioQueueBufferRef buf);
+  void ProcessPeaks();
+  static unsigned long GetCurrentTime();
   
-  SoundAnalyzer(UInt32 threshold);
-  
+  SoundAnalyzer(UInt32 threshold, AudioStreamBasicDescription* recordFormat);
+  ~SoundAnalyzer();
 };
 
 
